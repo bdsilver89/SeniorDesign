148733baa2de4d6495c828a0320cbce830fa72a5
bdsilver89 <brian.silver89@gmail.com>
2017-12-17 20:21:10 -0500

    implemented rapidxml library for RecipeML prototype

--- RecipeML_prototype/gen.cpp
+++ RecipeML_prototype/gen.cpp
@@ -1,65 +1,94 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <string>
+#include <iterator>
 #include "ingredient.h"
-
-
-void displayXML(std::string inputFilePath)
-{
-	std::string line;
-	std::ifstream InputFile(inputFilePath);
-
-	if (InputFile.is_open())
-	{
-	    while (getline(InputFile, line))
-        {	
-            std::cout << line << std::endl;
-        }
-    InputFile.close();
-    }
-
-    else
-    {
-        std::cout << "Could not open XML file" << std::endl;
-    }
-}
+#include "rapidxml-1.13/rapidxml.hpp"
+#include "rapidxml-1.13/rapidxml_print.hpp"
 
 
 void writeRecipe(std::string name, std::vector<Ingredient> list)
 {
 	std::string filePath = name;
 	filePath.append(".xml");
-    std::ofstream File(filePath);
+	rapidxml::xml_document<> doc;
+
+
+	rapidxml::xml_node<> *decl = doc.allocate_node(rapidxml::node_declaration);
+	decl->append_attribute(doc.allocate_attribute("version", "1.0"));
+	decl->append_attribute(doc.allocate_attribute("encoding", "UTF-8"));
+	doc.append_node(decl);
+
+	rapidxml::xml_node<> *doct = doc.allocate_node(rapidxml::node_doctype, "",
+												"recipeml PUBLIC \"-//FormatData//DTD RecipeML 0.5//EN\"\n"
+												"\t\"http://www.formatdata.com/recipeml.recipeml.dtd\"");
+	doc.append_node(doct);
+
+	rapidxml::xml_node<> *style = doc.allocate_node(rapidxml::node_pi, "xml-stylesheet",
+													"href=\"dessert1.css\" type=\"text/css\"");
+	doc.append_node(style);
+
+	rapidxml::xml_node<> *root = doc.allocate_node(rapidxml::node_element, "recipeml");
+	root->append_attribute(doc.allocate_attribute("version", "0.5"));
+	doc.append_node(root);
+
+	rapidxml::xml_node<> *recipe = doc.allocate_node(rapidxml::node_element, "recipe");
+	root->append_node(recipe);
+
+	rapidxml::xml_node<> *head = doc.allocate_node(rapidxml::node_element, "head");
+	rapidxml::xml_node<> *title = doc.allocate_node(rapidxml::node_element, "title", "thing");
+	head->append_node(title);
+	recipe->append_node(head);
+
+	rapidxml::xml_node<>* ingredients = doc.allocate_node(rapidxml::node_element, "ingredients");
+	recipe->append_node(ingredients);
+
+	rapidxml::xml_node<> *entry, *amt, *qty, *unit, *item;
+	for(std::vector<Ingredient>::iterator i = list.begin(); i != list.end(); ++i)
+	{
+		std::stringstream ss;
+		ss << (*i).getAmount();
+		std::string amount_str = ss.str();
+		char * amount = new char[amount_str.size() + 1];
+		std::copy(amount_str.begin(), amount_str.end(), amount);
+		amount[amount_str.size()] = '\0';
+
+		std::string meas_str = (*i).getMeasurement();
+		char * meas = new char[meas_str.size() + 1];
+		std::copy(meas_str.begin(), meas_str.end(), meas);
+		meas[meas_str.size()] = '\0';
+
+		std::string spice_str = (*i).getSpice();
+		char * spice = new char[spice_str.size() + 1];
+		std::copy(spice_str.begin(), spice_str.end(), spice);
+		spice[spice_str.size()] = '\0';
+
+
+		entry = doc.allocate_node(rapidxml::node_element, "ing");
+		ingredients->append_node(entry);
+
+		amt = doc.allocate_node(rapidxml::node_element, "amt");
+		entry->append_node(amt);
+
+		qty = doc.allocate_node(rapidxml::node_element, "qty", amount);
+		amt->append_node(qty);
+
+		unit = doc.allocate_node(rapidxml::node_element, "unit", meas);
+		amt->append_node(unit);
+
+		item = doc.allocate_node(rapidxml::node_element, "item", spice);
+		entry->append_node(item);
+	}
+	
+
+	std::ofstream File(filePath);
     if (File.is_open())
     {
-    	File << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    	File << "<!DOCTYPE recipeml PUBLIC \"-//FormatData//DTD RecipeML 0.5//EN\"\n";
-    	File << "\t\"http://www.formatdata.com/recipeml.recipeml.dtd\">\n";
-    	File << "<?xml-stylesheet href=\"dessert1.css\" type=\"text/css\"?>\n";
-		File << "<recipeml version=\"0.5\">\n";
-		File << "\t<recipe>\n";
-		File << "\t\t<head>\n";
-		File << "\t\t\t<title>";
-		File << name << "</title>\n";
-		File << "\t\t</head>\n";
-		File << "\t\t<ingredients>\n";
-
-	    for(std::vector<Ingredient>::iterator i = list.begin(); i != list.end(); ++i)
-	    {
-	        File << "\t\t\t<ing>\n";
-	        File << "\t\t\t\t<amt>\n";
-	        File << "\t\t\t\t\t<qty>" << (*i).getAmount() << "</qty>\n";
-	        File << "\t\t\t\t\t<unit>" << (*i).getMeasurement() << "</unit>\n";
-	        File << "\t\t\t\t</amt>\n";
-	        File << "\t\t\t\t<item>" << (*i).getSpice() << "</item>\n";
-	        File << "\t\t\t</ing>\n";
-	    }
-
-		File << "\t\t</ingredients>\n";
-
-		File << "\t</recipe>\n";
-		File << "</recipeml>";
-        File.close();
+    	File << doc;
+    	File.close();
+    	doc.clear();
     }
 
     else
@@ -70,25 +99,85 @@ void writeRecipe(std::string name, std::vector<Ingredient> list)
 
 
 
+std::vector<Ingredient> readRecipe(std::string name)
+{
+	std::vector<Ingredient> result;
+	std::string line;
+	std::string filePath = name;
+	filePath.append(".xml");
+	std::ifstream File(filePath);
+    if (File.is_open())
+    {
+		rapidxml::xml_document<> doc;
+		rapidxml::xml_node<> *root_node;
+
+	    std::vector<char> buffer((std::istreambuf_iterator<char>(File)), std::istreambuf_iterator<char>());
+	    buffer.push_back('\0');
+	    doc.parse<0>(&buffer[0]);
+	    root_node = doc.first_node("recipeml")->first_node("recipe");
+		
+		for(rapidxml::xml_node<> *i = root_node->first_node("ingredients")->first_node("ing");
+			i != 0; i = i->next_sibling("ing"))
+		{
+			std::string spice_str  (i->first_node("item")->value());
+			std::string meas_str   (i->first_node("amt")->first_node("unit")->value());
+			std::string amount_str (i->first_node("amt")->first_node("qty")->value());
+			double amount_d  = std::stod(amount_str);
+
+			Ingredient ingredient_obj(amount_d, meas_str, spice_str);
+			result.push_back(ingredient_obj);
+		}
+
+		File.close();
+	}
+
+    else
+    {
+        std::cout << "Could not read recipe" << std::endl;
+    }
+    return result;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
 	std::vector<Ingredient> IngredientList;
+	int choice;
 	std::string input;
-	std::cout << "Which recipe to write?" << std::endl;
-	std::cin  >> input;
 
-	Ingredient A(2  , "cups", "flour");
-	Ingredient B(1  , "tsp" , "salt");
-	Ingredient C(0.5, "oz"  , "butter");
+	std::cout << "Read ('0') or Write ('1') a Recipe?" << std::endl;
+	std::cin  >> choice;
 
-	IngredientList.push_back(A);
-	IngredientList.push_back(B);
-	IngredientList.push_back(C);
+	if(choice == 1)
+	{ 
+		std::cout << "Which recipe to write?" << std::endl;
+		std::cin  >> input;
 
+		Ingredient A(2.1256  , "cups", "flour");
+		Ingredient B(1.0  , "tsp" , "salt");
+		Ingredient C(0.5, "oz"  , "butter");
 
-	writeRecipe(input, IngredientList);
+		IngredientList.push_back(A);
+		IngredientList.push_back(B);
+		IngredientList.push_back(C);
 
-	//displayXML(input);
+		writeRecipe(input, IngredientList);
+	}
+
+	else if (choice == 0)
+	{
+		std::cout << "Which recipe to read?" << std::endl;
+		std::cin  >> input;
+		std::vector<Ingredient> items = readRecipe(input);
+		for(std::vector<Ingredient>::iterator s = items.begin(); s != items.end(); ++s)
+		{
+			std::cout << (*s).getSpice() << std::endl;
+			std::cout << (*s).getMeasurement() << std::endl;
+			std::cout << (*s).getAmount() << std::endl;
+			std::cout << std::endl;
+		}
+	}
 	return 0;
 }
 
