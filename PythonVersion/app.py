03042bdad2ee4be710fe8d1042d367e972047a4c
bdsilver89 <github@briansilver.net>
2018-04-28 05:13:40 -0400

    worked on flask server for front-end

--- PythonVersion/app.py
+++ PythonVersion/app.py
@@ -1,10 +1,39 @@
-from __future__ import division
-import time
+#from Tkinter import Tk
+#import GUI
+#import Recipe
+from flask import Flask, render_template, request
+from data import RecipeLib
 
-from Tkinter import Tk
-import GUI
-import Recipe
+app = Flask(__name__)
 
+Recipes = RecipeLib()
+
+@app.route('/')
+def index():
+   return render_template('home.html')
+
+
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html', recipes=Recipes)
+
+
+@app.route('/recipe/<string:id>/', methods=['GET', 'POST'])
+def recipe(id):
+    if request.method == 'POST':
+        val = request.form['text']
+        print("Submission: " + val)
+    return render_template('recipe.html', id=id)
+
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+'''
 root = Tk()
 my_gui = GUI.Application(root)
 root.mainloop()
+'''
---
+++ PythonVersion/data.py
@@ -0,0 +1,25 @@
+def RecipeLib():
+    recipes = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius quos officiis sint rerum voluptatum eligendi dicta deleniti totam molestias. Ipsa blanditiis illo neque, tenetur eius inventore officiis nesciunt aspernatur.',
+            'author': 'brian',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius quos officiis sint rerum voluptatum eligendi dicta deleniti totam molestias. Ipsa blanditiis illo neque, tenetur eius inventore officiis nesciunt aspernatur.',
+            'author': 'jason',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius quos officiis sint rerum voluptatum eligendi dicta deleniti totam molestias. Ipsa blanditiis illo neque, tenetur eius inventore officiis nesciunt aspernatur.',
+            'author': 'dan',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return recipes
